fca7e8f2afaf0f81bf5b8a61b35eae3a19286bc7
Alexey <nafiguspofigus@gmail.com>
2018-01-06 01:04:29 +0300

    Update and rename eve_odd to eve_odd.c

--- eve_odd.c
+++ eve_odd.c
@@ -15,3 +15,22 @@ int main(void){
     //system("pause>null");
     return 0;
 }
+
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    system("chcp 1251>0");
+    int number;
+    printf("Введите число!: \n");
+    scanf("%d", &number);
+    
+    if (number%2 == 0) printf("even\n");
+    if (number%2 == 1) printf("odd\n");
+    
+    scanf("pause>0");
+    return 0;
+}
+*/
