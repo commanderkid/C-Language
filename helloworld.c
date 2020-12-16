4091798e18a16d13611d80ea101c4c9851b9f2c0
Alexey <nafiguspofigus@gmail.com>
2020-12-16 16:46:28 +0300

    Carnigan and Richi Book
    I hope that finish this title one and fo ever!
    God speed!
    My first try!

---
+++ helloworld.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* vriant 1
+int main() {
+    printf("Hello world!");
+    return 0;
+}
+*/
+
+/* variant 2
+void main() {
+    printf("Hello World 2!!");
+}
+*/
+
+// variant 3
+int main(void) {
+    printf("Helloworld 3!!!");
+}
+
+// variant 3 - the best (in my honest opineon)
+/*
+void main(void) {
+    printf("Hello World unlimited!");
+}
+/*
