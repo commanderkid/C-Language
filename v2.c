98c54471594eeb346d49d0c54487a8104624ca90
Alexey <nafiguspofigus@gmail.com>
2021-01-03 16:13:26 +0300

    Create v2.c

---
+++ v2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void) {
+    // Задать шрифт консоли
+    system("chcp 1251>nul");
+    printf("Привет программа на С");
+    //Пауза в консоле
+    system("pause>null");
+}
